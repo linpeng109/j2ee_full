cc528c9af5b3d377ecb53c051a8f626bcbe27539
linpeng109 <linpeng109@aliyun.com>
2015-08-15 17:54:18 +0800

    cassandra test

--- src/test/java/test/cassandra/TestCassandra.java
+++ src/test/java/test/cassandra/TestCassandra.java
@@ -3,16 +3,17 @@ package test.cassandra;
 import com.cn.cassandra.tables.Person;
 import com.cn.common.RandomModule;
 import com.cn.common.implement.RandomModuleImpl;
+import com.datastax.driver.core.Token;
 import com.datastax.driver.core.querybuilder.QueryBuilder;
 import com.datastax.driver.core.querybuilder.Select;
+import org.apache.cassandra.service.pager.QueryPagers;
 import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.springframework.data.cassandra.core.CassandraTemplate;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import test.TestBase;
 
 import javax.annotation.Resource;
+import java.util.List;
 import java.util.UUID;
 
 /**
@@ -36,16 +37,37 @@ public class TestCassandra extends TestBase {
         }
     }
 
-    public void testCassandraTemplateCounter(){
+    public void testCassandraTemplateCounter() {
 
     }
 
+    // @Test
+    public void testCassandraTemplateCount() {
+        long size = cassandraTemplate.count("person");
+        logger.debug(String.format("The person table size is [%s] ", size));
+    }
+
     @Test
-    public void testCassandraTemplateSelect() {
+    public void testCassandraTemplateByLimt() {
+
         Select select = QueryBuilder.select().from("person");
-        UUID uuid=UUID.fromString("ee0bad9f-06f0-4a3a-bf7d-40985678278a");
+
+        select.limit(100);
+
+        List<Person> list = cassandraTemplate.select(select, Person.class);
+        for (int i = 0; i < list.size(); ++i) {
+            Person person = list.get(i);
+            logger.debug(String.format("name[%s]=[%s]", i, person.getName()));
+        }
+
+    }
+
+    //@Test
+    public void testCassandraTemplateSelectOne() {
+        Select select = QueryBuilder.select().from("person");
+        UUID uuid = UUID.fromString("ee0bad9f-06f0-4a3a-bf7d-40985678278a");
         select.where(QueryBuilder.eq("id", uuid));
         Person person = cassandraTemplate.selectOne(select, Person.class);
-        logger.debug(String.format("The persionid is %s and name is %s", person.getId(), person.getName()));
+        logger.debug(String.format("The persionid is [%s] and name is [%s]", person.getId(), person.getName()));
     }
 }
